3a21d5f42f28d114f0c5d27e25e49ecf8be611a3
Krzysztof <krzyspanfilewicz@gmail.com>
2025-10-02 19:08:21 +0200

    added add window

--- src/Main.java
+++ src/Main.java
@@ -14,14 +14,14 @@ import java.util.Vector;
 
 public class Main {
 
-    private JFrame frame;
+    private static JFrame frame;
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> new Main().createAndShowGUI());
     }
 
     private static JButton getjButton(JTextField textField, DefaultTableModel model) {
-        JButton button = new JButton("Dodaj Panstwo");
+        JButton button = new JButton("Ok");
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -33,6 +33,46 @@ public class Main {
                 v.add(country);
                 v.add(link);
                 model.addRow(v);
+            }
+        });
+        return button;
+    }
+
+    private static JButton getjButton1( DefaultTableModel model) {
+        JButton button = new JButton("Dodaj kraj");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JFrame frame1 = new JFrame("Input");
+                frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame1.setSize(500, 200);
+                frame1.setLayout(new BorderLayout(8, 8));
+
+                JPanel basicPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+
+                frame1.add(basicPanel, BorderLayout.NORTH);
+                JTextField textField = new JTextField(15);
+                JButton button1 = getjButton(textField, model);
+                JButton button2 = getjButton2(frame1);
+                JLabel label = new JLabel("Podaj nazwę kraju:");
+                JLabel label1 = new JLabel(new ImageIcon("znak_zapytania.png"));
+                basicPanel.add(label1);
+                basicPanel.add(label);
+                basicPanel.add(textField);
+                basicPanel.add(button1);
+                basicPanel.add(button2);
+
+                frame1.setVisible(true);
+            }
+        });
+        return button;
+    }
+    private static JButton getjButton2(JFrame frame ) {
+        JButton button = new JButton("Cancel");
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               frame.dispose();
             }
         });
         return button;
@@ -67,17 +107,15 @@ public class Main {
 
 
         frame.add(basicPanel, BorderLayout.SOUTH);
-        JTextField textField = new JTextField(15);
-        DefaultTableModel model = new DefaultTableModel();
 
-        ArrayList<ArrayList<String>> list = new ArrayList<>();
-        JButton button1 = getjButton(textField, model);
-        basicPanel.add(button1);
+        DefaultTableModel model = new DefaultTableModel();
+        JButton btn=getjButton1(model);
+        basicPanel.add(btn);
+       //
+
         model.addColumn("Flaga");
         model.addColumn("Kraj");
-        model.addColumn("Wiki");
-        basicPanel.add(new JLabel("Pole tekstowe:"));
-        basicPanel.add(textField);
+        model.addColumn("Wikipedia");
         String country = "Polska";
         Vector<Object> v = new Vector<>();
         Icon flag=new ImageIcon("Flaga_"+country+".png");
@@ -86,7 +124,7 @@ public class Main {
         v.add(country);
         v.add(link);
         model.addRow(v);
-        frame.add(basicPanel, BorderLayout.NORTH);
+        frame.add(basicPanel, BorderLayout.SOUTH);
 
 
 
